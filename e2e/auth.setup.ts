9fff86d4a13a5b81297968e06ec5f7ba6f7a0c4e
Michael Timmons <michael.timmmons@protonmail.com>
2025-01-11 13:31:47 -0500

    fix/e2e-config

--- e2e/auth.setup.ts
+++ e2e/auth.setup.ts
@@ -4,19 +4,16 @@ const TEST_EMAIL = process.env.TEST_EMAIL!
 const TEST_PASSWORD = process.env.TEST_PASSWORD!
 
 async function globalSetup() {
-    if( process.env.NODE_ENV === 'development' ) {
-        const supabase = createTestClient()
-        const { data, error } = await supabase.auth.signUp({
-            email: TEST_EMAIL,
-            password: TEST_PASSWORD
-        })
-    
-        if (error) {
-            console.error('Setup error:', error)
-        } else {
-            console.log('Test user created:', data)
-        }
+    const supabase = createTestClient()
+    const { data, error } = await supabase.auth.signUp({
+        email: TEST_EMAIL,
+        password: TEST_PASSWORD
+    })
 
+    if (error) {
+        console.error('Setup error:', error)
+    } else {
+        console.log('Test user created:', data)
     }
 }
 
--- playwright.config.ts
+++ playwright.config.ts
@@ -7,9 +7,7 @@ import { defineConfig, devices } from '@playwright/test';
 import dotenv from 'dotenv';
 import path from 'path';
 
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config({ path: path.resolve(__dirname, '.env.local') });
-}
+dotenv.config({ path: path.resolve(__dirname, '.env.local') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -29,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.NODE_ENV != "development" ? 'https://www.hft.studio' : 'http://localhost:3000',
+    baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -51,32 +49,12 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
