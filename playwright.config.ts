f81a3eebb358c532a14ef482fc08784fab98eaa1
Michael Timmons <michael.timmmons@protonmail.com>
2025-01-10 23:03:31 -0500

    Fix base url

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.NODE_ENV != "development" ? 'https://hft.studio' : 'http://localhost:3000',
+    baseURL: process.env.NODE_ENV != "development" ? 'https://www.hft.studio' : 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
